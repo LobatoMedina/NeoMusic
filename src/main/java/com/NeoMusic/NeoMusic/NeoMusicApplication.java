f7c5bf6f94a4b9086b0818fcd69f635841cb3112
Luis Eduardo Lobato <luislobato.medinaeduardo@gmail.com>
2026-04-14 19:48:42 -0600

    initial commit

---
+++ src/main/java/com/NeoMusic/NeoMusic/NeoMusicApplication.java
@@ -0,0 +1,13 @@
+package com.NeoMusic.NeoMusic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeoMusicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeoMusicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/NeoMusic/NeoMusic/NeoMusicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.NeoMusic.NeoMusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeoMusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
